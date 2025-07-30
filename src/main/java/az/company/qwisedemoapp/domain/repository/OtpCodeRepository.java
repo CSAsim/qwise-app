e594550b0ada51a7ebada81afaba0b4eecbd3c5a
CSAsim <asimpenahov6@gmail.com>
2025-07-30 10:41:17 +0400

    resend otp functionality added successfully

--- src/main/java/az/company/qwisedemoapp/domain/repository/OtpCodeRepository.java
+++ src/main/java/az/company/qwisedemoapp/domain/repository/OtpCodeRepository.java
@@ -1,7 +1,11 @@
 package az.company.qwisedemoapp.domain.repository;
 
 import az.company.qwisedemoapp.domain.entity.OtpCode;
+import az.company.qwisedemoapp.model.enums.OtpCodeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,5 +17,8 @@ public interface OtpCodeRepository extends JpaRepository<OtpCode, Long> {
 
     List<OtpCode> findByExpirationDateTimeBefore(LocalDateTime expirationDateTimeBefore);
     Optional<OtpCode> findByCode(String code);
-    Optional<OtpCode> findUserByCode(String otpCode);
+
+    @Modifying
+    @Query("UPDATE OtpCode o SET o.status = :status WHERE o.user.id = :userId")
+    void updateOtpCodeStatusByUserId(@Param("userId") Long userId, @Param("status") OtpCodeStatus status);
 }
