3951fd8c60bdd69f3fd1d6d5108391433ae9200b
Asim <asimpenahov6@gmail.com>
2025-11-08 00:17:26 +0400

    refresh token expiration day: 1 day

--- src/main/java/az/company/qwisedemoapp/service/auth/RefreshTokenService.java
+++ src/main/java/az/company/qwisedemoapp/service/auth/RefreshTokenService.java
@@ -26,7 +26,7 @@ public class RefreshTokenService {
         RefreshToken token = new RefreshToken();
         token.setToken(UUID.randomUUID().toString());
         token.setUser(user);
-        token.setExpiryDate(LocalDateTime.now().plusDays(7)); // 7 gün ömür
+        token.setExpiryDate(LocalDateTime.now().plusDays(1));
         token.setStatus(RefreshTokenStatus.ACTIVE);
         return refreshTokenRepository.save(token);
     }
