68f7a5b388745d2c158b41acf99ce607062bcaf8
Asim <asimpenahov6@gmail.com>
2025-11-19 00:47:41 +0400

    some changes for production

--- src/main/java/az/company/qwisedemoapp/QwiseDemoAppApplication.java
+++ src/main/java/az/company/qwisedemoapp/QwiseDemoAppApplication.java
@@ -3,10 +3,13 @@ package az.company.qwisedemoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class QwiseDemoAppApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Baku"));
         SpringApplication.run(QwiseDemoAppApplication.class, args);
     }
 }
